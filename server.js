1ed6a05afa5f2e0c0218344a3d903fe048508052
snehaarasid25 <snehuaraseed555@gmail.com>
2025-11-20 18:05:20 +0530

    cors error fixed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://companies-directory-nu.vercel.app/",
+      "https://companies-directory-nu.vercel.app",
       "http://localhost:3000",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
